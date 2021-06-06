8f3a1daf3f1184e27c81a2f3027f392884743533
Oliver Heamon <oliverheamon@gmail.com>
2021-06-06 17:18:46 +0100

    removed unused import

--- commons-bukkit/src/main/java/gg/eris/commons/bukkit/util/ParticleUtil.java
+++ commons-bukkit/src/main/java/gg/eris/commons/bukkit/util/ParticleUtil.java
@@ -7,7 +7,6 @@ import net.minecraft.server.v1_8_R3.EnumParticle;
 import net.minecraft.server.v1_8_R3.PacketPlayOutWorldParticles;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 @UtilityClass
