9c869aa3c6093a1778a8a01f71b95ba01459fe7d
alfiejfs <alfie@alfiesmith.net>
2021-06-13 04:07:24 +0100

    Broadcast

--- commons-bukkit/src/main/java/gg/eris/commons/bukkit/text/TextController.java
+++ commons-bukkit/src/main/java/gg/eris/commons/bukkit/text/TextController.java
@@ -14,6 +14,7 @@ import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.ComponentBuilder.FormatRetention;
 import net.md_5.bungee.api.chat.HoverEvent;
 import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 @UtilityClass
@@ -33,6 +34,10 @@ public final class TextController {
     for (Player player : players) {
       player.spigot().sendMessage(built);
     }
+  }
+
+  public static void broadcast(Builder builder) {
+    send(builder, Bukkit.getOnlinePlayers());
   }
 
   public static TextController.Builder builder(String rawText, TextType textType, Object... variables) {
