8a0f5fdf721135f62449a2a3ee480f4790293143
alfiejfs <alfie@alfiesmith.net>
2021-08-12 17:36:38 +0100

    update scoreboard in pipeline

--- commons-bukkit/src/main/java/gg/eris/commons/bukkit/impl/tablist/TablistControllerImpl.java
+++ commons-bukkit/src/main/java/gg/eris/commons/bukkit/impl/tablist/TablistControllerImpl.java
@@ -69,6 +69,7 @@ public final class TablistControllerImpl implements TablistController {
   public void updatePlayer(Player player) {
     updateDisplayNameOfPlayer(player);
     updateDisplayNamesForPlayer(player);
+    orderScoreboard(player);
   }
 
   @Override
